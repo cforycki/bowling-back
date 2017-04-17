5ae7a3aa15293f3f039c676251390bcd67fada39
cforycki <forycki.christophe@gmail.com>
2017-04-17 17:26:51 +0200

    Delete app.js

--- src/app.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-app.use('/api/games', require('./routes/games'));
-app.listen(3000, function () {
-    console.log('Listening on port 3000...');
-});
